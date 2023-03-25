48fd04b07fc50b4a12b325e25c414bf55a307a90
Shriram Patil <shrirampatil246@gmail.com>
2023-03-25 16:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+async function fetchData(callback) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    callback(data);
+  }
+  
+  function displayMessage(data) {
+      document.getElementById("output").innerText = data.title;
+  }
